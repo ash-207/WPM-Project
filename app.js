4c29a6c84f195a7c1cdba7d8fbe23e27b3275589
ash-207 <aayushvjn74025@gmail.com>
2025-08-20 22:50:45 +0530

    Website Prototype Uploaded

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import path from "path";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// Serve static files
+app.use(express.static("Webpages"));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "Webpages", "landing_page.html"));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "Webpages", "about_page.html"));
 });
 
-module.exports = app;
+app.get("/explore", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "Webpages", "explore.html"));
+});
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "Webpages", "dashboard.html"));
+});
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
